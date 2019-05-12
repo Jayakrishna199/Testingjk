88e1b8102a18f2bf79b19a4358ff152620beefb6
RK <RK@DESKTOP-OQ7QNAS>
2019-05-12 10:29:45 +0530

    Practice jk1 Added done

---
+++ Taskjk/src/jk1/A.java
@@ -0,0 +1,18 @@
+package jk1;
+
+public class A {
+	 int a=10;
+public int b=20;
+
+
+public void m1() {
+	System.out.println(a+b);
+}
+	
+
+public static void main(String[] args) {
+A obj= new A();
+obj.m1();
+	}
+
+}
